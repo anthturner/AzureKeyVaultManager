6ab41da7fe6301ba6f6597b813c34779d78abf9a
Eric Maino <ericmai@microsoft.com>
2016-02-05 13:52:35 -0800

    Adding basic contracts for interacting with KeyVault

---
+++ AzureKeyVault.Library/Contracts/IKeyVaultManagementService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AzureKeyVaultManager.Contracts
+{
+    public interface IKeyVaultManagementService
+    {
+        Task CreateKeyVault(string vaultName, string resourceGroup, string location);
+        Task<IKeyVault> GetKeyVault(string resourceGroup, string name, CancellationToken cancellationToken);
+        Task<ICollection<IKeyVault>> GetKeyVaults(string resourceGroup, CancellationToken cancellationToken);
+    }
+}
---
+++ AzureKeyVault.Library/Contracts/IKeyVaultService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AzureKeyVaultManager.Contracts
+{
+    public interface IKeyVaultService
+    {
+        Task<ICollection<IKeyVaultSecret>> GetSecrets(IKeyVault vault);
+    }
+}
