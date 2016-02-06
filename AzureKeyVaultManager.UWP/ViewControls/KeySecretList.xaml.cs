0bef5753137d42fb00d74f1d4b339e57253a0d6d
Anthony Turner <anthturn@microsoft.com>
2016-02-05 17:49:44 -0800

    Functional UI through listing vaults, keys, secrets

--- AzureKeyVaultManager.UWP/ViewControls/KeySecretList.xaml.cs
+++ AzureKeyVaultManager.UWP/ViewControls/KeySecretList.xaml.cs
@@ -1,26 +1,44 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using AzureKeyVaultManager.Contracts;
+using AzureKeyVaultManager.UWP.Annotations;
 
 namespace AzureKeyVaultManager.UWP.ViewControls
 {
-    public sealed partial class KeySecretList : UserControl
+    public sealed partial class KeySecretList : UserControl, INotifyPropertyChanged
     {
         public event EventHandler<IKeyVaultSecret> SelectionChanged;
-        public static readonly DependencyProperty SelectedProperty = DependencyProperty.Register("Selected", typeof(IKeyVaultSecret), typeof(KeySecretList), new PropertyMetadata(null));
-
-        public ObservableCollection<IKeyVaultSecret> KeysSecretsSource { get; set; } 
-
-        public IKeyVaultSecret Selected { get { return _selected; } set { _selected = value; } }
-        private IKeyVaultSecret _selected;
-
+        
         public KeySecretList()
         {
-            KeysSecretsSource = new ObservableCollection<IKeyVaultSecret>();
             this.InitializeComponent();
             this.DataContext = this;
+        }
+        
+        private ObservableCollection<IKeyVaultSecret> keysSecretsSource;
+        public ObservableCollection<IKeyVaultSecret> KeysSecretsSource
+        {
+            get { return keysSecretsSource; }
+            set { keysSecretsSource = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0)
+                SelectionChanged?.Invoke(this, (IKeyVaultSecret)e.AddedItems.Single());
         }
     }
 }
